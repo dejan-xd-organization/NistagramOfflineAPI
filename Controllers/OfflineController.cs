ee15230aa1ef4e896f68f4f7a6df16c909725154
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-21 15:12:24 +0200

    Close #1

---
+++ Controllers/OfflineController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NistagramOfflineAPI.Services;
+using NistagramSQLConnection.Model;
+using NistagramUtils.DTO;
+using NistagramUtils.DTO.WallPost;
+
+namespace NistagramOfflineAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OfflineController : ControllerBase
+    {
+
+        private readonly IOfflineService _iOfflineService;
+        private readonly IMapper _mapper;
+
+        public OfflineController(IOfflineService iOfflineService, IMapper mapper)
+        {
+            _iOfflineService = iOfflineService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("/[action]")]
+        public Object FilterUser(string filter)
+        {
+            List<User> user = _iOfflineService.FilterUser(filter);
+            List<UserDTO> userDTO = _mapper.Map<List<UserDTO>>(user);
+            return JsonConvert.SerializeObject(userDTO);
+        }
+
+        [HttpGet]
+        [Route("/[action]")]
+        public Object FindNewUsers()
+        {
+            List<User> user = _iOfflineService.FindNewUsers();
+            List<UserDTO> userDTO = _mapper.Map<List<UserDTO>>(user);
+            return JsonConvert.SerializeObject(userDTO);
+        }
+
+        [HttpGet]
+        [Route("/[action]")]
+        public Object GetAllPosts()
+        {
+            List<WallPost> post = _iOfflineService.GetAllWallPosts();
+            List<WallPostDTO> postDTO = _mapper.Map<List<WallPostDTO>>(post);
+            return JsonConvert.SerializeObject(postDTO);
+        }
+    }
+}
---
+++ Services/OfflineServiceImpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NistagramSQLConnection.Model;
+using NistagramSQLConnection.Service.Interface;
+
+namespace NistagramOfflineAPI.Services
+{
+    public class OfflineServiceImpl : IOfflineService
+    {
+
+        private readonly IPostService _iPostService;
+        private readonly IUserService _iUserService;
+
+        public OfflineServiceImpl(IPostService iPostService, IUserService iUserService)
+        {
+            _iPostService = iPostService;
+            _iUserService = iUserService;
+        }
+
+        public List<User> FilterUser(string filter)
+        {
+            return _iUserService.FilterUser(filter);
+        }
+
+        public List<User> FindNewUsers()
+        {
+            return _iUserService.FindNewUsers();
+        }
+
+        public List<WallPost> GetAllWallPosts()
+        {
+            return _iPostService.GetAllWallPosts(false);
+        }
+    }
+}
